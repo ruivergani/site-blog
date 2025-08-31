6c9bbfc52ca1942d01bc104f3227d3d82d777d41
Rui Vergani Neto <ruiverganineto@gmail.com>
2025-08-31 16:22:57 -0300

    Structure NextJS Course

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-purple-500">About</h2>
+    </div>
+  );
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-purple-500">Blog</h2>
+    </div>
+  );
+}
