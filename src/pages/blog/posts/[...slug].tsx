433bd65261e2c8dff8a447e22b032a1d2329c232
Rui Vergani Neto <ruiverganineto@gmail.com>
2025-08-31 22:03:33 -0300

    New structure course

--- src/pages/blog/posts/[...slug].tsx
+++ src/pages/blog/posts/[...slug].tsx
@@ -3,10 +3,14 @@ import { useRouter } from "next/router";
 export default function PostPage() {
 
   const router = useRouter();
+  const segments = router.query.slug as string[];
+
   console.log(router.query.slug)
   return (
     <div>
-      <h2 className="text-2xl font-bold text-purple-500">PostPage</h2>
+      <h2 className="text-2xl font-bold text-purple-500">
+        post: {segments?.join("/")}
+      </h2>
     </div>
   );
 }
